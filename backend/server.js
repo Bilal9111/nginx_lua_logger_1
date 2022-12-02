dca6fc40bb432252958e5d3f5d9139c3d5b12588
Bilal Chaudhry <mbc000@live.com>
2022-12-02 15:04:53 +0500

    backend only

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,6 @@
-const ronin 		= require( 'ronin-server' )
-const mocks 		= require( 'ronin-mocks' )
-const database	= require( 'ronin-database' )
-
-async function main() {
-
-	try {
-
-    const server = ronin.server({
-			port: process.env.PORT || 8080
-		})
-
-		server.use( '/services/m/', mocks.server( server.Router(), false, true ) )
-
-    const result = await server.start()
-    console.info( result )
-
-	} catch( error ) {
-		console.error( error )
-	}
-
-}
-
-function shutdown( signal ) {
-	console.info( `[${signal}] shutting down...` )
-	process.exit()
-}
-
-process.on( 'SIGINT', () => shutdown( 'SIGINT' ) )
-process.on( 'SIGTERM', () => shutdown( 'SIGTERM' ) )
-
-main()
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(req.url);
+  res.end();
+}).listen(8080);
